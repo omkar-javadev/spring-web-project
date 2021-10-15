b79989c91d5af2c973a8a9f5aad5049e34e748a4
omkar-javadev <omkar.javadev@gmail.com>
2021-10-16 00:08:25 +0530

    Code pushed first time

---
+++ src/test/java/com/example/jenkin/web/api/SpringWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkin.web.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
